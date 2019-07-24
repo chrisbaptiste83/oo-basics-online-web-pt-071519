d886fb3fcaa4c3bced3e70ca615344f4dbe09aa4
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-24 06:57:05 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,11 @@
 class Book
-  attr_accessor :author, :page_count, :genre, :title
+  attr_accessor :author, :page_count, :genre
   attr_reader :title
  
   def initialize(title)
     @title = title
   end
+  
   def title
     @title
   end
